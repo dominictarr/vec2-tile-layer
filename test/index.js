521b28f7e7b6a9a8215d410fa66505879ed6ac26
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-14 19:36:03 +0800

    update tests to new api, should have done this before publishing, oops

--- test/index.js
+++ test/index.js
@@ -3,9 +3,9 @@ var Rec2 = require('rec2')
 var tape = require('tape')
 
 tape('layer1 gets the right tiles', function (t) {
-  var l = new Layer(1)
-  var mapBounds = new Rec2().set(-180, -90)
-  mapBounds.bound.set(180, 90)
+  var l = new Layer({scale: 1})
+  var mapBounds = new Rec2().set(0, 0)
+  mapBounds.bound.set(1000, 1000)
   console.log(l.tileRange(mapBounds))
   t.deepEqual(l.minTile.toJSON(), {x:0, y:0})
   t.deepEqual(l.maxTile.toJSON(), {x:1, y:1})
@@ -13,19 +13,20 @@ tape('layer1 gets the right tiles', function (t) {
 })
 
 tape('layer1 gets the right tiles 2', function (t) {
-  var l = new Layer(1)
-  var mapBounds = new Rec2(0, 0)
-  mapBounds.bound.set(180, 90)
-  console.log(l.tileRange(mapBounds))
-  t.deepEqual(l.minTile.toJSON(), {x:1, y:1})
-  t.deepEqual(l.maxTile.toJSON(), {x:1, y:1})
+  var l = new Layer({scale: 1})
+  var mapBounds = new Rec2().set(500, 500)
+  mapBounds.bound.set(1000, 1000)
+  var b = l.tileRange(mapBounds)
+  console.log(b)
+  t.deepEqual(b.minTile.toJSON(), {x:1, y:1}, 'min tile')
+  t.deepEqual(b.maxTile.toJSON(), {x:1, y:1}, 'max tile')
   t.end()
 })
 
 tape('layer1 gets the right tiles 3', function (t) {
-  var l = new Layer(1)
-  var mapBounds = new Rec2().set(-180, -90)
-  mapBounds.bound.set(360, 180)
+  var l = new Layer({scale: 1})
+  var mapBounds = new Rec2().set(0, 0)
+  mapBounds.bound.set(1000, 1000)
   var b = l.tileRange(mapBounds)
   console.log(b)
   t.deepEqual(l.minTile.toJSON(), {x:0, y:0})
@@ -36,9 +37,9 @@ tape('layer1 gets the right tiles 3', function (t) {
 
 tape('layer2', function (t) {
   console.log('layer2')
-  var l = new Layer(2)
-  var mapBounds = new Rec2().set(-180, -90)
-  mapBounds.bound.set(360, 180)
+  var l = new Layer({scale: 2})
+  var mapBounds = new Rec2().set(0, 0)
+  mapBounds.bound.set(1000, 1000)
   var b = l.tileRange(mapBounds)
   t.deepEqual(b.minTile.toJSON(), {x:0, y:0})
   t.deepEqual(b.maxTile.toJSON(), {x:3, y:3})
@@ -48,9 +49,9 @@ tape('layer2', function (t) {
 
 tape('layer2', function (t) {
   console.log('layer2')
-  var l = new Layer(2)
-  var mapBounds = new Rec2().set(-90, -45)
-  mapBounds.bound.set(90, 45)
+  var l = new Layer({scale: 2})
+  var mapBounds = new Rec2().set(250, 250)
+  mapBounds.bound.set(750, 750)
   var b = l.tileRange(mapBounds)
   t.deepEqual(b.minTile.toJSON(), {x:1, y:1})
   t.deepEqual(b.maxTile.toJSON(), {x:2, y:2})
