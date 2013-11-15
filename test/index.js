d5330f85466e263e050b859007cd18e972324619
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-15 14:04:33 +0800

    add tests

--- test/index.js
+++ test/index.js
@@ -12,6 +12,43 @@ tape('layer1 gets the right tiles', function (t) {
   t.end()
 })
 
+
+tape('layer1 gets the right tiles - real example', function (t) {
+  var l = new Layer({scale: 1})
+  var mapBounds = new Rec2().set(298, 179)
+  mapBounds.bound.set(621, 501)
+  console.log(l.tileRange(mapBounds))
+  t.deepEqual(l.minTile.toJSON(), {x:0, y:0})
+  t.deepEqual(l.maxTile.toJSON(), {x:1, y:1})
+  t.end()
+})
+
+
+tape('layer1 gets the right tiless - real example2', function (t) {
+  var l = new Layer({scale: 1})
+  var mapBounds = new Rec2().set(-161, 159)
+  mapBounds.bound.set(521, 481)
+  console.log(l.tileRange(mapBounds))
+  t.deepEqual(l.minTile.toJSON(), {x:0, y:0})
+  t.deepEqual(l.maxTile.toJSON(), {x:1, y:0})
+  t.end()
+})
+
+
+
+tape('layer1 gets the right tiless - real example3', function (t) {
+  var l = new Layer({scale: 1})
+  var mapBounds = new Rec2().set(161, -341)
+  mapBounds.bound.set(841, 19)
+  console.log(l.tileRange(mapBounds))
+  t.deepEqual(l.minTile.toJSON(), {x:0, y:0}, 'min tile')
+  t.deepEqual(l.maxTile.toJSON(), {x:1, y:0}, 'max tile')
+  t.end()
+})
+
+
+
+
 tape('layer1 gets the right tiles 2', function (t) {
   var l = new Layer({scale: 1})
   var mapBounds = new Rec2().set(500, 500)
@@ -54,7 +91,7 @@ tape('layer2', function (t) {
   mapBounds.bound.set(750, 750)
   var b = l.tileRange(mapBounds)
   t.deepEqual(b.minTile.toJSON(), {x:1, y:1})
-  t.deepEqual(b.maxTile.toJSON(), {x:2, y:2})
+  t.deepEqual(b.maxTile.toJSON(), {x:3, y:3})
   t.end()
 })
 
